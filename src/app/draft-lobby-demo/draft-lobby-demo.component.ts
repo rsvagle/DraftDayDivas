dd43de8ca6098cee90e822fa5b94a861b031807a
rsvagle <rsvagle@gmail.com>
2025-03-05 20:34:22 -0600

    starting layout

--- src/app/draft-lobby-demo/draft-lobby-demo.component.ts
+++ src/app/draft-lobby-demo/draft-lobby-demo.component.ts
@@ -3,11 +3,17 @@ import { DraftService } from './draft-lobby.service';
 import { Subscription } from 'rxjs';
 import { PrimeNgLightModule } from '../primeng.light.module';
 import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TabViewModule } from 'primeng/tabview';
+import { AvatarModule } from 'primeng/avatar';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-draft-lobby-demo',
   standalone: true,
-  imports: [PrimeNgLightModule, CommonModule],
+  imports: [PrimeNgLightModule, CommonModule, CardModule, ToolbarModule, TabViewModule, AvatarModule, RippleModule, InputSwitchModule],
   providers: [DraftService],
   templateUrl: './draft-lobby-demo.component.html',
   styleUrl: './draft-lobby-demo.component.scss'
@@ -16,12 +22,19 @@ export class DraftLobbyDemoComponent {
   messages: { username: string; text: string }[] = [];
   newMessage: string = '';
   draftService = inject(DraftService);
+  selectedActivity: string = 'all';
+
   private draftSubscription: Subscription;
 
-  constructor() {}
+  constructor() {
+
+
+  }
 
   ngOnInit() {
     this.draftService.connect('draft_room');  // Replace with actual room name
+
+    // Subscribe to the backend pushing messages
     this.draftSubscription = this.draftService.getMessages().subscribe((message) => {
       this.messages.push(message);
     });
@@ -39,4 +52,6 @@ export class DraftLobbyDemoComponent {
     this.draftSubscription.unsubscribe();
     this.draftService.close();
   }
+
+
 }
